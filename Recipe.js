b4c00c8f4faae051267e591aa705e22136b45bac
Tintu Mariuyam Paul <60310275+TintuMariyamPaul@users.noreply.github.com>
2024-11-04 09:37:26 +0000

    commit

--- Recipe.js
+++ Recipe.js
@@ -1,9 +1,9 @@
-const recipe = {
-    title,
-    category,
-    ingredients,
-    steps,
-    cookingTime,
-    SpiceLevel,
-    CookingMethod
-}
+const newRecipe = {
+        title: document.getElementById('recipe-title').value,
+        category: document.getElementById('category').value,
+        ingredients: document.getElementById('ingredients').value,
+        steps: document.getElementById('steps').value,
+        cookingTime: document.getElementById('cooking-time').value,
+        spiceLevel: document.getElementById('spice-level').value,
+        cookingMethod: document.getElementById('cooking-method').value
+    };
