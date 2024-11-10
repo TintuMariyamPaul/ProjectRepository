beae91e8379e76c833144e3f5bcda28e150629bd
Tintu Mariuyam Paul <60310275+TintuMariyamPaul@users.noreply.github.com>
2024-11-10 22:06:42 +0000

    mades changes in html and javascript

--- Recipe.js
+++ Recipe.js
@@ -1,4 +1,28 @@
-const newRecipe = {
+let recipe = []
+
+function display() {
+    const recipe_List = document.getElementById('recipelist');
+    recipe_List.innerHTML = '';
+
+    recipe.forEach((recipes,index) => {
+        const result = document.createElement('div');
+        result.innerHTML = `
+        ${recipes.title} - 
+        ${recipes.category} - 
+        ${recipes.ingredients} - 
+        ${recipes.steps} - 
+        ${recipes.cookingTime} min - 
+        ${recipes.cookingMethod} - 
+        ${recipes.spiceLevel}
+    `;
+    recipe_List = append(result);
+    
+    });
+}
+
+document.getElementById('add_recipe').addEventListener('click', () => {
+
+    const newRecipe = {
         title: document.getElementById('recipe-title').value,
         category: document.getElementById('category').value,
         ingredients: document.getElementById('ingredients').value,
@@ -6,4 +30,4 @@ const newRecipe = {
         cookingTime: document.getElementById('cooking-time').value,
         spiceLevel: document.getElementById('spice-level').value,
         cookingMethod: document.getElementById('cooking-method').value
-    };
+    }
